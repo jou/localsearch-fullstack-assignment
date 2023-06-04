a417c077b50279d1337ccffd98296f030c772741
Jiayong Ou <jou@orly.ch>
2023-06-04 15:38:49 +0200

    Add TypeScript support for BFF

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
     extends: [
-        './vendor/jou-style-guide/eslint-node'
-    ]
-}
+        './vendor/jou-style-guide/eslint-node',
+        './vendor/jou-style-guide/eslint-typescript',
+    ],
+    root: true,
+};
