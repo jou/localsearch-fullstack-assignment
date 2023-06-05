1e7277d9304054f14ddb6bbc74acf4e32c902b18
Jiayong Ou <jou@orly.ch>
2023-06-05 11:34:02 +0200

    fix vue ts eslint

--- vendor/jou-style-guide/eslint-typescript.js
+++ vendor/jou-style-guide/eslint-typescript.js
@@ -1,27 +1,28 @@
 module.exports = {
-    "overrides": [
+    overrides: [
         {
-            "files": ["**/*.ts", "**/*.tsx"],
-            "parser": "@typescript-eslint/parser",
-            "extends": [
-                "plugin:@typescript-eslint/recommended"
-            ],
-            "rules": {
-                "@typescript-eslint/no-non-null-assertion": "off",
-                "@typescript-eslint/no-use-before-define": "off",
-                "@typescript-eslint/no-warning-comments": "off",
-                "@typescript-eslint/no-empty-function": "off",
-                "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-                "@typescript-eslint/explicit-function-return-type": "off",
-                "@typescript-eslint/camelcase": "off",
-                "node/no-missing-import": "off",
-                "node/no-empty-function": "off",
-                "node/no-unsupported-features/es-syntax": "off",
-                "node/no-missing-require": "off",
-                "node/shebang": "off",
-                "no-dupe-class-members": "off",
-                "require-atomic-updates": "off"
-            }
-        }
-    ]
-}
+            files: ['**/*.ts', '**/*.tsx', '**/*.vue'],
+            parser: '@typescript-eslint/parser',
+            extends: ['plugin:@typescript-eslint/recommended'],
+            rules: {
+                '@typescript-eslint/no-non-null-assertion': 'off',
+                '@typescript-eslint/no-use-before-define': 'off',
+                '@typescript-eslint/no-warning-comments': 'off',
+                '@typescript-eslint/no-empty-function': 'off',
+                '@typescript-eslint/no-unused-vars': [
+                    'error',
+                    { argsIgnorePattern: '^_' },
+                ],
+                '@typescript-eslint/explicit-function-return-type': 'off',
+                '@typescript-eslint/camelcase': 'off',
+                'node/no-missing-import': 'off',
+                'node/no-empty-function': 'off',
+                'node/no-unsupported-features/es-syntax': 'off',
+                'node/no-missing-require': 'off',
+                'node/shebang': 'off',
+                'no-dupe-class-members': 'off',
+                'require-atomic-updates': 'off',
+            },
+        },
+    ],
+};
