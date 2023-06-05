a229886037a24f2ab24fff518f7ca8481d0568d3
Jiayong Ou <jou@orly.ch>
2023-06-05 18:39:29 +0200

    Enable CORS for everyone in BFF

--- places-bff/src/app.ts
+++ places-bff/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors';
 
 import placesRouter from './routes/places';
 
@@ -11,6 +12,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/places', placesRouter);
