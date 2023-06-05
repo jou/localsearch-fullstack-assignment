074827cce1e8bbe6a73802fae494a1bbd31c6a94
Jiayong Ou <jou@orly.ch>
2023-06-05 10:03:33 +0200

    Use Tailwind CSS for styling

--- places-web/README.markdown
+++ places-web/README.markdown
@@ -9,6 +9,9 @@ This section collects notes and assumptions made for the web app.
 ### Bootstrapping process
 
 -   Create new [Vite][] app as per [its guide][vite-guide] with `vue-ts` as template & reformatted output with prettier
+-   Add [Tailwind CSS][tailwindcss] as per its [instructions for Vite][tailwindcss-vite].
 
 [Vite]: https://vitejs.dev
 [vite-guide]: https://vitejs.dev/guide/
+[tailwindcss]: https://tailwindcss.com
+[tailwindcss-vite]: https://tailwindcss.com/docs/guides/vite
--- places-web/src/components/HelloWorld.vue
+++ places-web/src/components/HelloWorld.vue
@@ -7,34 +7,19 @@ const count = ref(0);
 </script>
 
 <template>
-    <h1>{{ msg }}</h1>
+    <div>
+        <h1 class="text-3xl mb-1 text-blue-800">{{ msg }}</h1>
 
-    <div class="card">
-        <button type="button" @click="count++">count is {{ count }}</button>
-        <p>
-            Edit
-            <code>components/HelloWorld.vue</code> to test HMR
-        </p>
+        <div>
+            <button
+                class="font-bold py-2 px-4 rounded bg-blue-800 text-white hover:bg-blue-400"
+                type="button"
+                @click="count++"
+            >
+                count is {{ count }} (Click me!)
+            </button>
+        </div>
     </div>
-
-    <p>
-        Check out
-        <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-            >create-vue</a
-        >, the official Vue + Vite starter
-    </p>
-    <p>
-        Install
-        <a href="https://github.com/vuejs/language-tools" target="_blank"
-            >Volar</a
-        >
-        in your IDE for a better DX
-    </p>
-    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-    color: #888;
-}
-</style>
+<style scoped></style>
